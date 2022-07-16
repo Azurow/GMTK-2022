fa7d8baa210cc5ce1433cd9d0e626cd7286bc2b3
Emil <MCAzurow@gmail.com>
2022-07-16 21:15:12 +0200

    Added Aim Preview, Added Varying Throwing Power
    Throwing Power now depends on how far away your coursor is, though its capped to MaxSpeed (on the Dice Prefab)

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -9,6 +9,8 @@ public class Weapon : MonoBehaviour
     public GameObject bulletPrefab;
     private Animator animator;
 
+    private bool isShooting;
+
     void Start()
     {
         animator = GetComponent<Animator>();
@@ -21,8 +23,31 @@ public class Weapon : MonoBehaviour
         var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
         firePoint.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 
-        if (Input.GetButtonDown("Fire1"))
+
+        //Shooting and shot preview mechanics
+        if(Input.GetButtonDown("Fire1"))
         {
+            isShooting = true;
+        }
+        
+        if(Input.GetButton("Fire2"))
+        {
+            isShooting = false;
+            GetComponent<LineRenderer>().enabled = false;
+        }
+
+        if (Input.GetButton("Fire1") && isShooting)
+        {
+            Vector3 mousePos = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y, 0);
+            GetComponent<LineRenderer>().enabled = true;
+            GetComponent<LineRenderer>().SetPosition(0, firePoint.transform.position);
+            GetComponent<LineRenderer>().SetPosition(1, (firePoint.transform.position + mousePos) / 2);
+            GetComponent<LineRenderer>().SetPosition(2, mousePos);
+        }
+
+        if(Input.GetButtonUp("Fire1") && isShooting)
+        {
+            GetComponent<LineRenderer>().enabled = false;
             Shoot();
         }
     }

c6fd6a0defab4366176e9ad4ce68ef25be6d921f
ozenImmovable <95392627+ozenImmovable@users.noreply.github.com>
2022-07-17 12:35:33 -0400

    Main Menu
    first pass of the main menu, with sounds and menu actions

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        StartCoroutine(ExampleCoroutine());
+        
+    }
+
+    IEnumerator ExampleCoroutine()
+    {
+        //Print the time of when the function is first called.
+        Debug.Log("Started Coroutine at timestamp : " + Time.time);
+
+        //yield on a new YieldInstruction that waits for 5 seconds.
+        yield return new WaitForSeconds(2);
+
+        SceneManager.LoadScene("DiceGameLVL1");
+
+    }
+}
